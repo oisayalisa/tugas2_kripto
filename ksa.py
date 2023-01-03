3a6005e8dcf2e38d0e984009d830cce3cbc1918b
asrisamsudin22 <stnurhaliza10@gmail.com>
2023-01-03 13:02:42 +0800

    up tugas 2

---
+++ ksa.py
@@ -0,0 +1,27 @@
+#E1E120051_SITTI NUR HALIZA_KSA
+s = []
+k = ['s','a','p','u','t','r','a','1']
+
+panjangk = len(k)
+for i in range(256):
+    s.append(i)
+print("s = ",s)
+
+j = 0
+for i in range(256):
+    key_index = k[i % panjangk]
+    j = (j + s[i] + ord(key_index)) % 256
+
+    temp = s[i]
+    s[i] = s[j]
+    s[j] = temp
+print()
+print("s = ",s)
+
+
+
+
+
+
+
+
